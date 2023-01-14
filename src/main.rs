ae24b9af193efcdf88a5af6e73c8ed55113342cb
Simon-Pierre Gingras <892367+spg@users.noreply.github.com>
2023-01-14 12:35:23 -0500

    get window size

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use libc::{c_int, c_ulong, winsize, STDOUT_FILENO};
+use std::mem::zeroed;
+
+extern "C" {
+    fn ioctl(fd: c_int, request: c_ulong, ...);
+}
+
+const TIOCGWINSZ: c_ulong = 0x40087468;
+
+unsafe fn get_dimensions() -> winsize {
+    let window: winsize = zeroed();
+    unsafe { ioctl(STDOUT_FILENO, TIOCGWINSZ, &window) };
+    window
+}
+
 fn main() {
-    println!("Hello, world!");
+    let w = unsafe { get_dimensions() };
+    println!("{}", w.ws_col);
+    println!("{}", w.ws_row);
 }
