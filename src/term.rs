f8db134120240c2a21ff0b52c41bb252626d818c
Simon-Pierre Gingras <892367+spg@users.noreply.github.com>
2023-04-10 10:44:54 -0400

    cleanup

--- src/term.rs
+++ src/term.rs
@@ -1,20 +1,6 @@
-use libc::{c_int, c_ulong, winsize, STDOUT_FILENO};
-use std::mem::zeroed;
-
-extern "C" {
-    fn ioctl(fd: c_int, request: c_ulong, ...);
-}
-
-const TIOCGWINSZ: c_ulong = 0x40087468;
-
-pub unsafe fn get_dimensions() -> winsize {
-    let window: winsize = zeroed();
-    unsafe { ioctl(STDOUT_FILENO, TIOCGWINSZ, &window) };
-    window
-}
-
 pub fn move_cursor_to_top() {
-    print!("\x1B[1;1H")
+    print!("\x1B[1;1H");
+    print!("{}[2J", 27 as char);
 }
 
 pub fn print_state(state: &Vec<Vec<bool>>) {
@@ -23,5 +9,6 @@ pub fn print_state(state: &Vec<Vec<bool>>) {
             let c = if cell.clone() == true { "*" } else { " " };
             print!("{}", c);
         }
+        print!("\n");
     }
 }
